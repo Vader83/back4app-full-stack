892e5fe4f4b96c8362c76fa240abcc3cf9e1c41e
Vader83 <enderbro07@gmail.com>
2025-06-19 15:05:47 +0300

    added env.var. check

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+if (!process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID) {
+  console.log('NEXT_PUBLIC_PARSE_APPLICATION_ID is missing');
+} else {
+  console.log('NEXT_PUBLIC_PARSE_APPLICATION_ID is set');
+}
+
+if (!process.env.NEXT_PUBLIC_PARSE_JAVASCRIPT_KEY) {
+  console.log('NEXT_PUBLIC_PARSE_JAVASCRIPT_KEY is missing');
+} else {
+  console.log('NEXT_PUBLIC_PARSE_JAVASCRIPT_KEY is set');
+}
+
+console.log('Available ENV keys:', Object.keys(process.env));
+
 module.exports = nextConfig;
